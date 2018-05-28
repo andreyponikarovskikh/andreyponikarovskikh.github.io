201045c3f4f8d68fd09c1685b1ca4fa1ef84e461
Andrey Ponikarovskikh <a.ponikarovskikh@gmail.com>
2018-05-28 11:57:35 +0500

    Test final

--- test/js/task.js
+++ test/js/task.js
@@ -1,113 +1,174 @@
-// объекты 
-var start = document.querySelector(".task-start");
-var popup = document.querySelector(".task-repeat");
-var taskSum = document.querySelector(".task-sum");
-var firstDenominator = document.querySelector(".first-denominator");
-var secondDenominator = document.querySelector(".second-denominator");
-var firstDenominatorInput = document.querySelector(".first-denominator-input");
-var secondDenominatorInput = document.querySelector(".second-denominator-input");
-
-// валидный диапазон чисел
-const A_MIN = 6; 
-const A_MAX = 9;
-const RANGE_A = A_MAX - A_MIN + 1; 
-
-const SUM_AB_MIN = 11; 
-const SUM_AB_MAX = 14;
-const RANGE_SUM_AB = SUM_AB_MAX - SUM_AB_MIN + 1;
-
-// вспомогательные переменные и константы
-var a;
-var b;
-var total;
-
-let getRandomInteger = max => Math.floor(Math.random() * (max));
-
-
-function getInitialValues (){
-	a = A_MIN + getRandomInteger(RANGE_A);
-    sumAB = SUM_AB_MIN + getRandomInteger(RANGE_SUM_AB);
-    b = sumAB - a;
-    firstDenominator.textContent = a;
-    secondDenominator.textContent = b;
-    taskSum.value = "?";
-    // console.log (a);
-    // console.log (b);
+//случайное целое число из диапазона
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+var a = getRandomInteger(6,9);
+var result = getRandomInteger(11,14);
+var b = result - a;
 
-start.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    firstDenominatorInput.classList.remove("hidden");
-    start.classList.add("hidden");
-    getInitialValues();
-    firstDenominatorInput.focus();
-  });
+slotA.innerHTML = a;
+slotB.innerHTML = b;
 
-firstDenominatorInput.addEventListener("keydown",function (evt) {
-    if (evt.key == "Enter") {
-    	if (firstDenominatorInput.value == a) {
-    		firstDenominatorInput.classList.add("js-input-right");
-    		firstDenominatorInput.style.color = "#272727";
-    		firstDenominatorInput.style.border = "none";
-    		firstDenominatorInput.setAttribute('readonly', true);
-    		firstDenominator.style.backgroundColor = "";
-    		secondDenominatorInput.classList.remove("hidden");
-    		secondDenominatorInput.focus();
-    	} else {
-    		firstDenominatorInput.classList.add("js-input-wrong");
-    		firstDenominator.style.backgroundColor = "#fca749";
-    		firstDenominator.style.borderRadius = "4px";
-    	}
-	};
+//параметры зависящие от размеров sprite.png (левый отступ, длина и высота дуги в 2 деления линейки)
+var arrowOneIndent = 39;
+var arrowMinLength = 75;
+var arrowMinHeight = 35;
+
+//расчет размеров стрелок
+function getArrowLength(AorB, isSecond) {
+  return (AorB-2)*arrowOneIndent+arrowMinLength + (isSecond ? 1 : 0);//+1px при переходе через жирную вертикальную линию линейки
+}
+
+function getArrowHeight(el) {
+  return getArrowTopPoint(el === inputOne ? a : b)*0.75;//коэф. 0.75 дает реальную высоту дуги относительно верхней точки кривой Безье
+}
+
+function getArrowTopPoint(AorB) {
+  return (AorB-2)*12+arrowMinHeight;//верхняя точка кривой Безье
+}
+
+//составление svg path для нужной длины стрелки
+function drawArrow(AorB, isSecond) {
+  var arrowLength = getArrowLength(AorB, isSecond);
+  var arrowTopPoint = getArrowTopPoint(AorB);
+  var startPoint = "M0,0 ";
+  var curverOne = "C" + arrowTopPoint/2 + "," + -arrowTopPoint + " ";
+  var curverTwo = arrowLength-arrowTopPoint/2 + "," + -arrowTopPoint + " ";
+  var endPoint = arrowLength + ",0 ";
+  var arrowHead = "l 0,-8 -6,4 6,3";
+  return "<path d='"+startPoint+curverOne+curverTwo+endPoint+arrowHead+"' style='"+dashing(AorB)+"'></path>";
+}
+
+//свойства для равномерной анимации отрисовки стрелок разной длины
+function dashing(AorB) {
+  var dash = (AorB+1)*50;
+  return "stroke-dasharray: "+dash+";stroke-dashoffset: "+dash+";"
+}
+
+//установка инпута для слагаемых над центром дуги
+function setInputPosition(el) {
+  el.style.left = (el !== inputOne ? arrowTwoIndent+(getArrowLength(b, true)/2) : getArrowLength(a)/2+
+  arrowOneIndent)-el.offsetWidth/2+"px";
+  el.style.top = -getArrowHeight(el)-12+"px"; //12px зависят от высоты инпута
+}
+
+arrowOne.innerHTML = drawArrow(a);
+setInputPosition(inputOne);
+inputOne.focus();
+
+//отступ от левого края линейки для второй стрелки
+var arrowTwoIndent = arrowOneIndent + getArrowLength(a);
+arrowTwoField.style.left = arrowTwoIndent+"px";
+
+//обработка нажатий клавиш
+window.addEventListener('keypress', function(e){
+  if (event.key !== " " && !isNaN(+event.key)){
+    if (typeof inputTwo !== 'undefined' && typeof slotResult === 'undefined') {
+      inputTwo.value = event.key;
+      handleInputValue(inputTwo, b);
+    } else if (typeof inputTwo !== 'undefined' && typeof slotResult !== 'undefined') {
+      slotResult.value += event.key;
+      handleInputValue(slotResult, result);
+    } else {
+      inputOne.value = event.key;
+      handleInputValue(inputOne, a);
+    }
+  }
+  if (event.key === " " && answerIsCorrect) {
+    location.reload();
+  }
+  e.preventDefault();
 });
 
-secondDenominatorInput.addEventListener("keydown",function (evt) {
-    if (evt.key == "Enter") {
-    	if (secondDenominatorInput.value == b) {
-    		secondDenominatorInput.classList.add("js-input-right");
-    		secondDenominatorInput.style.color = "#272727";
-    		secondDenominatorInput.style.border = "none";
-    		secondDenominatorInput.setAttribute('readonly', true);
-    		secondDenominator.style.backgroundColor = "";
-    		taskSum.removeAttribute('readonly');
-    		taskSum.classList.remove("readonly");
-    		taskSum.value = "";
-    		taskSum.style.outline = "none";
-    		taskSum.focus();
-    	} else {
-    		secondDenominatorInput.classList.add("js-input-wrong");
-    		secondDenominator.style.backgroundColor = "#fca749";
-    		secondDenominator.style.borderRadius = "4px";
-    	}
-	};
-});
+//действия при копировании в инпуты
+window.addEventListener('input', function(){
+  if (event.target.id === 'inputOne') {
+    handleInputValue(inputOne, a); 
+  }
+  if (event.target.id === 'inputTwo') {
+    handleInputValue(inputTwo, b);
+  }
+  if (event.target.id === 'slotResult') {
+    handleInputValue(slotResult, result);
+  }
+})
 
-taskSum.addEventListener("keydown",function (evt) {
-    // console.log (taskSum.value);
-    if (evt.key == "Enter") {
-    	if (taskSum.value == sumAB) {
-    		taskSum.style.border = "none";
-    		taskSum.setAttribute('readonly', true);
-    		taskSum.style.color = "#272727";
-    		popup.classList.remove("hidden");
-    		popup.classList.add("js-visible-button");
-    		// popup.focus();
-    	} else {
-    		taskSum.classList.add("js-input-wrong");
-    	}
-	};
-});
+var answerIsCorrect = false; //используется как условие для перезагрузки страницы
+
+//обработка введенных значений
+function handleInputValue(input, targetValue) {
+  if (input.value == targetValue) {
+    if (input === inputOne) {
+      slotA.classList.remove('incorrect');
+      arrowTwo.innerHTML = drawArrow(b,true);
+      addInput('inputTwo');
+    } else if (input === inputTwo) {
+      slotB.classList.remove('incorrect');
+      addInput('slotResult');
+      slotResult.focus();
+    } else {
+      answerIsCorrect = true;
+      congratulate();
+    }
+    inputToNumber(input);
+  } else {
+    if (input.value.length >= targetValue.toString().length) {
+      input.style.color = 'red';
+    }
+    if (input === inputOne) {
+      slotA.classList.add('incorrect');
+    } 
+    if (typeof inputTwo !== 'undefined' && input === inputTwo) {
+      slotB.classList.add('incorrect');
+    }
+    if (targetValue === result && input.value.length > 2) {
+      input.value = input.value.charAt(2);
+      input.style.color = '';
+    }
+  }
+}
+
+//конвертация инпут в текст
+function inputToNumber(input) {
+  var id = input.id;
+  var value = input.value;
+  var span = document.createElement("span");
+  span.id = id;
+  span.innerHTML = value;
+  if (typeof slotResult !== 'undefined' && input === slotResult) {
+    input.remove();
+    numbers.appendChild(span);
+  } else {
+    input.remove();
+    axis.appendChild(span);
+    setInputPosition(span);
+  }
+}
+
+//добавление нового инпута
+function addInput(id) {
+  var newInput = document.createElement("input");
+  newInput.type = 'text';
+  newInput.id = id;
+  if (id === 'inputTwo') {
+    axis.appendChild(newInput);
+    setInputPosition(inputTwo, true);
+    inputTwo.focus();
+  } else {
+    question.remove();
+    numbers.appendChild(newInput);
+  }
+}
+
+//вывод сообщения после решения примера
+
+var popup = document.querySelector(".task-repeat");
+function congratulate() {
+  popup.classList.remove("hidden");
+  popup.classList.add("button-visible");
+}
 
 popup.addEventListener("click", function (evt) {
-    // evt.preventDefault();
-    // popup.classList.add("hidden");
-    // getInitialValues();
-    // firstDenominatorInput.focus();
     location.reload(); // window.location.reload()
   });
-
-
-
-
-
